740448f31fe874cbaa432546f81999bd3be4c289
ana gonzález <ana@MacBook-de-ana.local>
2019-02-14 21:57:53 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FriendFinder
+Friend Finder will help you meet with people based on things in common. So, answer the questions honestly!# FriendFinder
---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var defaultArray = [
+    {
+        "name": "Ana Banana",
+        "photo": "https://www.instagram.com/p/BHyG3mHhxFi/?utm_source=ig_web_button_share_sheet",
+        "scores": [
+            5,
+            2,
+            4,
+            4,
+            5,
+            5,
+            2,
+            5,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "Bailarina Tai-tai",
+        "photo": "https://www.instagram.com/p/-Q7HFSM8GQ/?utm_source=ig_web_button_share_sheet",
+        "scores": [
+            1,
+            1,
+            2,
+            3,
+            1,
+            4,
+            3,
+            5,
+            5,
+            2
+        ]
+    },
+
+];
+module.exports = defaultArray;
